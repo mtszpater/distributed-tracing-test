867eb95198ef8e6ebc7d429221a7585969c4dea5
Mateusz Pater <mtszpater@gmail.com>
2020-02-22 13:39:40 +0100

    create zipkin server

---
+++ zipkin-server/src/main/java/dev/pater/zipkin/ZipkinServer.java
@@ -0,0 +1,15 @@
+package dev.pater.zipkin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableZipkinServer
+public class ZipkinServer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZipkinServer.class, args);
+	}
+
+}
